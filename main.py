4269fb3e53ef686fc5f418dbea8bfa96bd32e06a
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2024-07-20 20:17:24 +0200

    startcode 24-25

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import pygame, time
 
 #
-# define constants 
+# definitions 
 #
 
 FPS = 30 # Frames Per Second
@@ -13,10 +13,6 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 BALL_WIDTH = 16
 BALL_HEIGHT = 16
-
-#
-# define global variables
-#
 
 ball_x = 0
 ball_speed_x = 6
@@ -34,11 +30,11 @@ fps_clock = pygame.time.Clock()
 # read images
 #
 
-spritesheet = pygame.image.load('Breakout_Tile_Free.png').convert_alpha() # convert_alpha increases speed of blit and keeps transparancy of .png
+spritesheet = pygame.image.load('Breakout_Tile_Free.png').convert_alpha()   
 
-ball_img = pygame.Surface((64, 64)) # create new image
-ball_img.blit(spritesheet, (0, 0), (1403, 652, 64, 64))  # copy part of sheet to image
-ball_img = pygame.transform.scale(ball_img, (BALL_WIDTH, BALL_HEIGHT)) # resize image
+ball_img = pygame.Surface((64, 64), pygame.SRCALPHA)  
+ball_img.blit(spritesheet, (0, 0), (1403, 652, 64, 64))   
+ball_img = pygame.transform.scale(ball_img, (BALL_WIDTH, BALL_HEIGHT))  
 
 #
 # game loop
@@ -47,28 +43,48 @@ ball_img = pygame.transform.scale(ball_img, (BALL_WIDTH, BALL_HEIGHT)) # resize 
 print('mygame is running')
 running = True
 while running:
-    # read all events
-    # to end programme when GUI is closed and to read keys using pygame.get.get_pressed()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
+    #
+    # read events
+    # 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:  
+            running = False 
+    keys = pygame.key.get_pressed() 
+            
+    # 
+    # move everything
+    #
+
     # move ball
     ball_x = ball_x + ball_speed_x
-    # bounce ball against edges of screen
-    if ball_x < 0 or ball_x + BALL_WIDTH > SCREEN_WIDTH:
-      ball_speed_x = ball_speed_x * -1
+
+    # bounce ball
+    if ball_x < 0 : 
+      ball_speed_x = abs(ball_speed_x) 
+    if ball_x + BALL_WIDTH > SCREEN_WIDTH: 
+      ball_speed_x = abs(ball_speed_x) * -1 
+
+    # 
+    # handle collisions
+    #
+    
+    # 
+    # draw everything
+    #
 
     # clear screen
     screen.fill('black') 
 
     # draw ball
     screen.blit(ball_img, (ball_x, 0))
-
+    
     # show screen
     pygame.display.flip() 
 
-    # Sleep the remaining time of this frame
-    fps_clock.tick(FPS) 
+    # 
+    # wait until next frame
+    #
+
+    fps_clock.tick(FPS) # Sleep the remaining time of this frame
 
 print('mygame stopt running')
